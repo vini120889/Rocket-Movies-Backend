8eaaf8aba9d9114a8357093cda86be71e1e8fdee
Vinícius Carvalho <vini.cius@uol.com.br>
2024-05-15 14:37:48 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http:127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http:127.0.0.1:5173", "https://rocketmoviescadastrodefilmes.netlify.app"],
     credentials: true,
 }));
 
