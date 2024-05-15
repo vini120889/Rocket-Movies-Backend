bf9d81098ff0e0c30bc399b7e091aced4d6831b7
Vinícius Carvalho <vini.cius@uol.com.br>
2024-05-15 14:18:10 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http:127.0.0.1:5173", "https://rocketmovies-alpha.vercel.app"],
+    origin: ["http://localhost:5173", "http:127.0.0.1:5173"],
     credentials: true,
 }));
 
